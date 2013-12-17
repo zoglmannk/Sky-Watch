eb83cd840551a64b882160f45371f6795f79b241
Kurt Zoglmann <zoglmannk@yahoo.com>
2013-12-16 19:12:32 -0600

    finish adding set of related unit tests for KZPebbleDataChunk

--- Sky WatchTests/KZPebbleDataChunkTests.m
+++ Sky WatchTests/KZPebbleDataChunkTests.m
@@ -106,4 +106,194 @@
     XCTAssertEqual(56, [encodedSunSetAzimuth intValue], @"The run set azimuth should be encoded correctly");
 }
 
+- (void)testEncodedResultContainsCorrectSolarNoon {
+    KZResult *result = [KZResult new];
+    result.sun = [KZEvent new];
+    result.sun.meridianCrossing = [[KZSimpleTime alloc] initWithHour:14 minute:9];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedSolarNoon = (NSNumber*) [encodedResult objectForKey:@(SOLAR_NOON)];
+    
+    int expected = 14*60 + 9;
+    XCTAssertEqual(expected, [encodedSolarNoon intValue], @"Solar noon should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectSolarMidnight {
+    KZResult *result = [KZResult new];
+    result.sun = [KZEvent new];
+    result.sun.antimeridianCrossing = [[KZSimpleTime alloc] initWithHour:2 minute:30];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedSolarMidnight = (NSNumber*) [encodedResult objectForKey:@(SOLAR_MIDNIGHT)];
+    
+    int expected = 2*60 + 30;
+    XCTAssertEqual(expected, [encodedSolarMidnight intValue], @"Solar midnight should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectGoldenHourBegin {
+    KZResult *result = [KZResult new];
+    result.goldenHour = [KZEvent new];
+    result.goldenHour.rise = [[KZSimpleTime alloc] initWithHour:10 minute:22];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedGoldenHourBegin = (NSNumber*) [encodedResult objectForKey:@(GOLDEN_HOUR_BEGIN_KEY)];
+    
+    int expected = 10*60 + 22;
+    XCTAssertEqual(expected, [encodedGoldenHourBegin intValue], @"The golden hour begin should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectGoldenHourEnd {
+    KZResult *result = [KZResult new];
+    result.goldenHour = [KZEvent new];
+    result.goldenHour.set = [[KZSimpleTime alloc] initWithHour:17 minute:02];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedGoldenHourEnd = (NSNumber*) [encodedResult objectForKey:@(GOLDEN_HOUR_END_KEY)];
+    
+    int expected = 17*60 + 2;
+    XCTAssertEqual(expected, [encodedGoldenHourEnd intValue], @"The golden hour end should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectCivilTwilightBegin {
+    KZResult *result = [KZResult new];
+    result.civilTwilight = [KZEvent new];
+    result.civilTwilight.rise = [[KZSimpleTime alloc] initWithHour:6 minute:10];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedCivilTwilightBegin = [encodedResult objectForKey:@(CIVIL_TWILIGHT_BEGIN_KEY)];
+    
+    int expected = 6*60 + 10;
+    XCTAssertEqual(expected, [encodedCivilTwilightBegin intValue], @"The civil twilight begin should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectCivilTwilightEnd {
+    KZResult *result = [KZResult new];
+    result.civilTwilight = [KZEvent new];
+    result.civilTwilight.set = [[KZSimpleTime alloc] initWithHour:18 minute:2];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedCivilTwilightEnd = [encodedResult objectForKey:@(CIVIL_TWILIGHT_END_KEY)];
+    
+    int expected = 18*60 + 2;
+    XCTAssertEqual(expected, [encodedCivilTwilightEnd intValue], @"The civil twilight end should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectNauticalTwilightBegin {
+    KZResult *result = [KZResult new];
+    result.nauticalTwilight = [KZEvent new];
+    result.nauticalTwilight.rise = [[KZSimpleTime alloc] initWithHour:5 minute:55];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedNauticalTwilightBegin = [encodedResult objectForKey:@(NAUTICAL_TWILIGHT_BEGIN_KEY)];
+    
+    int expected = 5*60 + 55;
+    XCTAssertEqual(expected, [encodedNauticalTwilightBegin intValue], @"The nautical twilight begin should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectNauticalTwilightEnd {
+    KZResult *result = [KZResult new];
+    result.nauticalTwilight = [KZEvent new];
+    result.nauticalTwilight.set = [[KZSimpleTime alloc] initWithHour:19 minute:13];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedNauticalTwilightEnd = [encodedResult objectForKey:@(NAUTICAL_TWILIGHT_END_KEY)];
+    
+    int expected = 19*60 + 13;
+    XCTAssertEqual(expected, [encodedNauticalTwilightEnd intValue], @"The nautical twilight end should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectAstronomicalTwilightBegin {
+    KZResult *result = [KZResult new];
+    result.astronomicalTwilight = [KZEvent new];
+    result.astronomicalTwilight.rise = [[KZSimpleTime alloc] initWithHour:4 minute:30];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedAstronomicalTwilightBegin = [encodedResult objectForKey:@(ASTRONOMICAL_TWILIGHT_BEGIN_KEY)];
+    
+    int expected = 4*60 + 30;
+    XCTAssertEqual(expected, [encodedAstronomicalTwilightBegin intValue], @"The astronomical twilight begin should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectAstronomicalTwilightEnd {
+    KZResult *result = [KZResult new];
+    result.astronomicalTwilight = [KZEvent new];
+    result.astronomicalTwilight.set = [[KZSimpleTime alloc] initWithHour:20 minute:15];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedAstronomicalTwilightEnd = [encodedResult objectForKey:@(ASTRONOMICAL_TWILIGHT_END_KEY)];
+    
+    int expected = 20*60 + 15;
+    XCTAssertEqual(expected, [encodedAstronomicalTwilightEnd intValue], @"The astronomical twilight end should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectMoonRise {
+    KZResult *result = [KZResult new];
+    result.moonToday = [KZMoonEvent new];
+    result.moonToday.rise = [[KZSimpleTime alloc] initWithHour:0 minute:5];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedMoonRise = [encodedResult objectForKey:@(MOON_RISE_KEY)];
+    
+    int expected = 0*60 + 5;
+    XCTAssertEqual(expected, [encodedMoonRise intValue], @"The moon rise should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectMoonRiseAzimuth {
+    KZResult *result = [KZResult new];
+    result.moonToday = [KZMoonEvent new];
+    result.moonToday.riseAzimuth = 123.823;
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedMoonRiseAzimuth = [encodedResult objectForKey:@(MOON_RISE_AZIMUTH_KEY)];
+    
+    XCTAssertEqual(124, [encodedMoonRiseAzimuth intValue], @"The moon rise azimuth should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectMoonSet {
+    KZResult *result = [KZResult new];
+    result.moonToday = [KZMoonEvent new];
+    result.moonToday.set = [[KZSimpleTime alloc] initWithHour:20 minute:50];
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedMoonSet = [encodedResult objectForKey:@(MOON_SET_KEY)];
+    
+    int expected = 20*60 + 50;
+    XCTAssertEqual(expected, [encodedMoonSet intValue], @"The moon set should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsCorrectMoomSetAzimuth {
+    KZResult *result = [KZResult new];
+    result.moonToday = [KZMoonEvent new];
+    result.moonToday.setAzimuth = 82.1;
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedMoonSetAzimuth = [encodedResult objectForKey:@(MOON_SET_AZIMUTH_KEY)];
+    
+    XCTAssertEqual(82, [encodedMoonSetAzimuth intValue], @"The moon set azimuth should be encoded correctly");
+}
+
+- (void)testEncodedResultContainsMoonAge {
+    KZResult *result = [KZResult new];
+    result.moonToday = [KZMoonEvent new];
+    result.moonToday.ageInDays = 13;
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedMoonAgeInDays = [encodedResult objectForKey:@(MOON_AGE_KEY)];
+    
+    XCTAssertEqual(13, [encodedMoonAgeInDays intValue], @"The moon age should be encoded correctly");
+}
+
+- (void)testEncodedResultContaisnPercentIllumination {
+    KZResult *result = [KZResult new];
+    result.moonToday = [KZMoonEvent new];
+    result.moonToday.illuminationPercent = 55.332;
+    
+    NSDictionary *encodedResult = [_dataChunk encodedResult:result slot:1];
+    NSNumber *encodedIllumination = [encodedResult objectForKey:@(MOON_PERCENT_ILLUMINATION)];
+    
+    int expected = 55;
+    XCTAssertEqual(expected, [encodedIllumination intValue], @"The moon's percent illumination should be encoded correctly");
+}
+
+
 @end
